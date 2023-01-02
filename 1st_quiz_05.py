66860f941e5258db2f546631cb1d9c8bc0768fda
Ji-yoon Park <parkjiyoon7@gmail.com>
2023-01-02 14:47:59 +0900

    Update 1st_quiz_05.py

--- 1st_quiz_05.py
+++ 1st_quiz_05.py
@@ -25,7 +25,7 @@
 
 def solution(arr):
     left, right = 0, len(arr)-1
-    while @@@:
+    while right > 0:
         arr[left], arr[right] = arr[right], arr[left]
         left += 1
         right -= 1
