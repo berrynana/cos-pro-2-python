a14eccc2b8f779e2f02633772dc9ba7e35a7dfbe
Ji-yoon Park <parkjiyoon7@gmail.com>
2023-01-02 16:12:57 +0900

    Update 2nd_quiz_03.py

--- 2nd/2nd_quiz_03.py
+++ 2nd/2nd_quiz_03.py
@@ -29,8 +29,10 @@
 #import math
 
 def solution(N, M):
-    #여기에 코드를 작성해주세요.
     answer = 0
+    for x in range(N, M + 1):
+        if x%2 == 0:
+            answer += x * x
     return answer
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
