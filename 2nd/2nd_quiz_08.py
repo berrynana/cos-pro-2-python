0d5df507de5e69604a0b3348001e4c663e66caec
Ji-yoon Park <parkjiyoon7@gmail.com>
2023-01-03 09:55:02 +0900

    Update 2nd_quiz_08.py

--- 2nd/2nd_quiz_08.py
+++ 2nd/2nd_quiz_08.py
@@ -28,7 +28,7 @@
 
 def solution(number):
     count = 0
-    while number >= 0:
+    while number: #0이 
         n = number % 10
         if n == 2 or n == 3 or n == 5 or n == 7:
             count += 1
